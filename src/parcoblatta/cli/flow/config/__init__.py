828f9921496743193ff1a2ca6e47251c1fd42e2f
Skyler Curtis <skylerr.curtis@gmail.com>
2026-08-26 04:45:22 -0400

    reorganizing

--- src/parcoblatta/cli/flow/config/__init__.py
+++ src/parcoblatta/cli/flow/config/__init__.py
@@ -1,6 +1,7 @@
-from parcoblatta.cli.flow.config.flow import Flow, FlowConfig, Rule, load_flow_config, load_flow_settings
+from parcoblatta.cli.flow.config.flow import Flow, Rule
 from parcoblatta.cli.flow.config.output import KafkaConfig, Output
 from parcoblatta.cli.flow.config.prompt import PromptTemplate
+from parcoblatta.cli.flow.config.pyproject import FlowConfig, load_flow_config, load_flow_settings
 from parcoblatta.scanner.input.code import CodeInput
 from parcoblatta.scanner.query.treesitter import QuerySpec, TreesitterQuery
 
